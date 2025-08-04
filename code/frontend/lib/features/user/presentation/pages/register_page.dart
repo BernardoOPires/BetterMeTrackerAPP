daaf96b936e5b6340f6f0720d6534017646856f4
BernardoOPires <bernardoopires14@gmail.com>
2025-08-04 11:02:00 -0300

    register page 1

--- code/frontend/lib/features/user/presentation/pages/register_page.dart
+++ code/frontend/lib/features/user/presentation/pages/register_page.dart
@@ -25,13 +25,13 @@ class RegisterPage extends StatelessWidget {
             children: [
               SizedBox(height: 80),
               Padding(
-                padding: EdgeInsets.all(20),
+                padding: EdgeInsets.all(15),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      "Register",
-                      style: TextStyle(color: Colors.white, fontSize: 40),
+                      "Create Your Account",
+                      style: TextStyle(color: Colors.white, fontSize: 30),
                     ),
                     SizedBox(height: 10),
                     Text(
@@ -49,40 +49,34 @@ class RegisterPage extends StatelessWidget {
                       topLeft: Radius.circular(60),
                       topRight: Radius.circular(60),
                     ),
-                    color: Colors.white,
+                    color: backgroundColor,
                   ),
                   padding: EdgeInsets.all(30),
                   child: Column(
                     children: [
-                      SizedBox(height: 60),
+                      SizedBox(height: 20),
                       Container(
                         decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                              // color: Color.fromRGBO(225, 95, 27, .3),
-                              color: primaryColor,
-                              blurRadius: 15,
-                              offset: Offset(0, 7),
-                            ),
-                          ],
                         ),
                         child: Column(
                           children: [
                             Container(
                               padding: EdgeInsets.all(5),
                               decoration: BoxDecoration(
+                                color: backgroundColor,
                                 border: Border(
-                                  bottom: BorderSide(
-                                    color: Colors.grey, //change to 200
-                                  ),
+                                  bottom: BorderSide(color: Colors.grey),
                                 ),
                               ),
                               child: TextField(
                                 decoration: InputDecoration(
                                   hintText: "Username",
-                                  hintStyle: TextStyle(color: Colors.grey),
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 15,
+                                  ),
                                   border: InputBorder.none,
                                 ),
                               ),
@@ -90,16 +84,18 @@ class RegisterPage extends StatelessWidget {
                             Container(
                               padding: EdgeInsets.all(5),
                               decoration: BoxDecoration(
+                                color: backgroundColor,
                                 border: Border(
-                                  bottom: BorderSide(
-                                    color: Colors.grey, //change to 200
-                                  ),
+                                  bottom: BorderSide(color: Colors.grey),
                                 ),
                               ),
                               child: TextField(
                                 decoration: InputDecoration(
                                   hintText: "Email",
-                                  hintStyle: TextStyle(color: Colors.grey),
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 15,
+                                  ),
                                   border: InputBorder.none,
                                 ),
                               ),
@@ -107,26 +103,32 @@ class RegisterPage extends StatelessWidget {
                             Container(
                               padding: EdgeInsets.all(5),
                               decoration: BoxDecoration(
+                                color: backgroundColor,
                                 border: Border(
-                                  bottom: BorderSide(
-                                    color: Colors.grey, //change to 200
-                                  ),
+                                  bottom: BorderSide(color: Colors.grey),
                                 ),
                               ),
                               child: TextField(
                                 decoration: InputDecoration(
-                                  hintText: "Email",
-                                  hintStyle: TextStyle(color: Colors.grey),
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 15,
+                                  ),
                                   border: InputBorder.none,
                                 ),
                               ),
                             ),
                             Container(
+                              decoration: BoxDecoration(color: backgroundColor),
                               padding: EdgeInsets.all(5),
                               child: TextField(
                                 decoration: InputDecoration(
-                                  hintText: "Passworld",
-                                  hintStyle: TextStyle(color: Colors.grey),
+                                  hintText: "Reapeat Password",
+                                  hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 15,
+                                  ),
                                   border: InputBorder.none,
                                 ),
                               ),
@@ -134,15 +136,7 @@ class RegisterPage extends StatelessWidget {
                           ],
                         ),
                       ),
-                      SizedBox(height: 40),
-                      Text(
-                        "Forgot Password?",
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 40),
+                      SizedBox(height: 70),
                       Container(
                         height: 50,
                         margin: EdgeInsets.symmetric(horizontal: 50),
@@ -152,7 +146,7 @@ class RegisterPage extends StatelessWidget {
                         ),
                         child: Center(
                           child: Text(
-                            "Login",
+                            "Register",
                             style: TextStyle(
                               color: Colors.white,
                               fontWeight: FontWeight.bold,
@@ -161,7 +155,7 @@ class RegisterPage extends StatelessWidget {
                         ),
                       ),
 
-                      SizedBox(height: 50),
+                      SizedBox(height: 30),
                       Text(
                         "Continue with social media",
                         style: TextStyle(
--- code/frontend/lib/shared/themes/colors.dart
+++ code/frontend/lib/shared/themes/colors.dart
@@ -1,10 +1,10 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFF4A90E2);
+const primaryColor = Color.fromARGB(255, 36, 163, 198);
 const secondaryColor = Color(0xFFFFA552);
 const supColor = Color(0xFF50E3C2);
-const backgroundColor = Color(0xFFF5F7FA);
+const backgroundColor = Color.fromARGB(255, 230, 237, 247);
 const lightBlueBackground = Color(0xFFEAF4FC);
 const primaryTxtColor = Color(0xFF333333);
 const secondaryTxtColor = Color(0xFF666666);
